6636c089dcd090477c6001ba94653b07ed9f66d2
Dmytro Khylko <dmytro.khylko@gmail.com>
2021-12-30 14:16:38 +0200

    Changed templates

--- greeting_app/models/visitor_model.py
+++ greeting_app/models/visitor_model.py
@@ -1,10 +1,14 @@
 from .db import db
+from sqlalchemy import inspect
 
 class Visitor(db.Model):
     __tablename__ = "visitors"
 
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False, unique=True)
+
+    def toDict(self):
+        return { c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs }
 
 
     @staticmethod
